473a99abd227349db81ddd95b6d30941ead3d7a6
DESKTOP-3JQOKBE\Batch 9 <gracia.masengi@gmail.com>
2024-02-26 10:21:34 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿class Program{
+    static void Main()
+    {
+        FooBar.DeretFooBar(15);
+    }
+}
+
+public static class FooBar{
+	public static string fooBarRow = "0, ";
+	public static string fooBarNum = "";
+	public static void DeretFooBar(int index){
+		for(int i = 1; i < index; i++){
+            
+			fooBarRow += NumToFooBar(i) + ", ";
+		fooBarRow += NumToFooBar(index);
+		}
+    }
+    public static string NumToFooBar(int num){
+        if(num % 3 == 0 && num % 5 != 0){
+            fooBarNum = "foo";
+        }
+        else if(num % 3 != 0 && num % 5 == 0){
+            fooBarNum = "bar";
+        }
+        else if(num % 3 == 0 && num % 5 == 0){
+            fooBarNum = "foobar"; 
+        }
+        else{
+            fooBarNum = num.ToString();
+        }
+        return fooBarNum;
+    }
+	Console.WriteLine(fooBarRow);
+}
